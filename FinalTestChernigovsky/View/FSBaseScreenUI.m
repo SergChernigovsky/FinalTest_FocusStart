f33e2c1eca056a12db7761409d661a2be8aa6a09
SergChernigovsky <amenti88@yandex.ru>
2017-03-29 14:42:38 +0700

    Revert "Revert "Delete animation""
    This reverts commit 26e1a754947959ac963ee2a48a54474780233c85.

--- FinalTestChernigovsky/View/FSBaseScreenUI.m
+++ FinalTestChernigovsky/View/FSBaseScreenUI.m
@@ -89,11 +89,7 @@
 {
     dispatch_async(dispatch_get_main_queue(), ^
     {
-        element.alpha = 0.f;
         [view addSubview:element];
-        [UIView animateWithDuration:0.5f animations:^{
-            element.alpha = 1.f;
-        }];
     });
     NSMutableArray *mutableElements = [arrayFinalElements mutableCopy];
     [mutableElements addObject:element];
